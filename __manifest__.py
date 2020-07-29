75003c71177b9e0df2a803aa798457a3dde0d197
eswaran <eswaranit95@gmail.com>
2020-07-29 14:07:10 +0530

    LIst VieW Sticky Header Source Code

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "List View Sticky Header",
+
+    'summary': """
+      List View Sticky Header v13.0""",
+
+    'description': """
+	- Sticky Header
+	- List Sticky Header
+	- Header Sticky
+	- Table Hedar Sticky
+	- ref : 'https://apps.odoo.com/apps/modules/13.0/listview_sticky_header/',
+    """,
+
+    'maintainer': 'Eswaran L<eswaranit95@gmail.com>,',
+    'author': "Eswaran",
+    'license': 'LGPL-3',
+    'category': 'Tools',
+    'version': '13.0.0.0.0 ',
+    'images': ['static/description/main.png'],
+    'depends': ['base'],
+
+    'data': [
+        'views/assets.xml',
+    ],
+
+}
